13db30e1388e3ee116e7186733fae32f3b7ffd38
Mathias Gartner <mathiasgartner@gmx.at>
2025-10-17 18:00:15 +0200

    user agent logging

--- app.js
+++ app.js
@@ -1,17 +1,55 @@
-var config = require('./config');
-let express = require('express');
-let path = require('path');
-let bodyParser = require("body-parser");
-var cors = require('cors')
-let app = express();
-//var http = require('http');
+const config = require('./config');
+const express = require('express');
+const path = require('path');
+const bodyParser = require("body-parser");
+const requestIp = require("request-ip");
+const UAParser = require("ua-parser-js");
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const sql = require('./app/models/db.js');
 
-app.use(cors()) 
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+app.use(cookieParser());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const MAX_COOKIE_AGE = 60 * 60 * 1000; // 1 hour
+
+app.use((req, res, next) => {
+    // Only log for root page
+    if (req.path === '/' && !req.cookies.firstVisitLogged) {
+        const ip = requestIp.getClientIp(req);
+        const parser = new UAParser(req.headers['user-agent']);
+        const ua = parser.getResult();
+
+        const query = "INSERT INTO interactivelog (Ip, Browser, OS, Device, UserAgent) VALUES (?, ?, ?, ?, ?)";
+        
+        const values = [
+            ip,
+            ua.browser.name || '',
+            ua.os.name || '',
+            ua.device.type || 'desktop',
+            req.headers['user-agent'] || ''
+        ];
+
+        sql.query(query, values, (err, result) => {
+            if (err) {
+                console.error('Failed to log client to database:', err);
+                console.log('client info:', values);
+            }
+        });
+
+        // Set a cookie so we don’t log this client again
+        res.cookie('firstVisitLogged', '1', { maxAge: MAX_COOKIE_AGE });
+    }
+    next();
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 require("./app/routes/box.routes.js")(app);
 require("./app/routes/check.routes.js")(app);
@@ -19,21 +57,11 @@ require("./app/routes/move.routes.js")(app);
 require("./app/routes/roll.routes.js")(app);
 require("./app/routes/schedule.routes.js")(app);
 
-/*
-http.createServer(function (req, res) {
-	console.log(req.httpVersion + '\n');
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(config.port, "0.0.0.0");*/
-
-app.get('/', function(req, res) {
-    //res.sendFile("index.html", { root: __dirname });
-    res.redirect('index.html');
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: path.join(__dirname, 'public') });
 });
 
-// set port, listen for requests
+// Start server
 app.listen(config.port, "0.0.0.0", () => {
-    console.log("Server is running on port " + config.port + ".");
+    console.log(`Server is running on port ${config.port}`);
 });
-
-console.log('Server running at http://0.0.0.0:' + config.port + '/');
